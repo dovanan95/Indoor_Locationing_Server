42d9dd6530af7585ce7f84c44509644faacb8e28
dovanan95 <dovanan95@gmail.com>
2021-03-25 15:05:37 +0900

    huhu

--- IDLS/settings.py
+++ IDLS/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import django_heroku
+import pymysql
 from pathlib import Path
+pymysql.version_info = (1, 3, 13, "final", 0)
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
